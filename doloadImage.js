b9cf81580577338869609630a8fbd85120af7c6e
AlexandrUkr <alexandr1drozd@gmail.com>
2015-10-22 08:36:18 +0300

    Create doloadImage.js

---
+++ doloadImage.js
@@ -0,0 +1,12 @@
+var doloadImage = {
+	check: function() {  
+		$('[data-src]').each(function(e){
+			if(LoadImage.is_view($(this)))
+				$(this).attr('src', $(this).attr('data-src')).removeAttr('data-src').hide().fadeIn("slow");
+		});  
+	},
+	is_view: function(e){
+		var wtop = $(window).scrollTop(), wbottom = wtop + $(window).height(), etop = $(e).offset().top, ebottom = etop + $(e).height();  
+		return ((ebottom >= wtop) && (etop <= wbottom));
+	}
+}
